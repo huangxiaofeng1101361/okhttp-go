f483fdb4bb4d9a637a6792982df73ade764ff81b
huangxiaofeng <xiaofeng.huang@fih-foxconn.com>
2019-08-26 11:01:55 +0800

    test first commit

---
+++ app/src/androidTest/java/com/hxf/okhttp_go/ExampleInstrumentedTest.java
@@ -0,0 +1,27 @@
+package com.hxf.okhttp_go;
+
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.hxf.okhttp_go", appContext.getPackageName());
+    }
+}
